672d95cb409860818bd0f62e18aa278e6bcec9dd
ABAcoder <67940201+ABAcoder@users.noreply.github.com>
2022-02-12 01:42:48 -0500

    Delete Meet_n_Eat_Final directory

--- Meet_n_Eat_Final/test.py
+++
@@ -1,6 +0,0 @@
-from cs50 import SQL
-
-db = SQL("sqlite:///meet_n_eat.db")
-
-if db.execute("SELECT temp_code FROM users WHERE username = ?", "test4571676259")[0]["temp_code"] != 0:
-    print("success")
